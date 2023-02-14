7f478906622d9c9bcdde6bcdbb1278872201e9de
Sagar <sagarxyadav@gmail.com>
2023-02-14 00:09:51 -0500

    run init.sh

--- init.sh
+++ init.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Install Rust
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+# curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 # Add the ARMv7 target
-rustup target add armv7-unknown-linux-gnueabihf
+# rustup target add armv7-unknown-linux-gnueabihf
 
 # Create a new Rust project
 cargo init
